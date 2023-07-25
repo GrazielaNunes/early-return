68da8ca4ce0bbd9ee675e8bf8abf48ad30a95443
grazi <grazielanunesj@gmail.com>
2023-07-25 14:23:06 -0300

    Aplicado o conceito de early return

---
+++ src/Cliente.java
@@ -0,0 +1,18 @@
+public class Cliente {
+
+    public String nome;
+
+    public int idade;
+
+    public String email;
+
+
+    public String endereco;
+    public String telefone;
+    public String cpf;
+    public String profissao;
+    public String nacionalidade;
+    public String enderecoTrabalho;
+    public String estadoCivil;
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,58 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import jdk.internal.icu.impl.NormalizerImpl;
+
 public class Main {
-    public static void main(String[] args) public class Cliente {
+    public static void main(String[] args) {
 
-        public boolean validarCliente() {
 
-            if (nome != null && !nome.isEmpty()) {
-                if (idade > 0 && idade <= 120) {
-                    if (email != null && email.contains("@")) {
-                        if (endereco != null && !endereco.isEmpty()) {
-                            if (telefone != null && !telefone.isEmpty()) {
-                                if (cpf != null && cpf.length() == 11) {
-                                    if (profissao != null && !profissao.isEmpty()) {
-                                        if (estadoCivil != null && !estadoCivil.isEmpty()) {
-                                            if (nacionalidade != null && !nacionalidade.isEmpty()) {
-                                                if (enderecoTrabalho != null && !enderecoTrabalho.isEmpty()) {
-                                                    return true;
-                                                } else {
-                                                    System.out.println("Endereço de trabalho inválido.");
-                                                }
-                                            } else {
-                                                System.out.println("Nacionalidade inválida.");
-                                            }
-                                        } else {
-                                            System.out.println("Estado civil inválido.");
-                                        }
-                                    } else {
-                                        System.out.println("Profissão inválida.");
-                                    }
-                                } else {
-                                    System.out.println("CPF inválido.");
-                                }
-                            } else {
-                                System.out.println("Telefone inválido.");
-                            }
-                        } else {
-                            System.out.println("Endereço inválido.");
-                        }
-                    } else {
-                        System.out.println("E-mail inválido.");
-                    }
-                } else {
-                    System.out.println("Idade inválida.");
-                }
-            } else {
-                System.out.println("Nome inválido.");
-            }
+    }
 
+    public static Boolean validarCliente(Cliente c) {
+
+        if (c.nome == null || c.nome.isEmpty()) {
+            System.out.println("Nome inválido.");
             return false;
-
         }
+
+        if (c.idade <= 0 || c.idade > 120) {
+            System.out.println("Idade inválida.");
+            return false;
+        }
+
+        if (c.endereco == null || c.endereco.isEmpty()) {
+            System.out.println("Endereço inválido.");
+            return false;
+        }
+
+        if (c.telefone == null || c.telefone.isEmpty()) {
+            System.out.println("Telefone inválido.");
+            return false;
+        }
+
+        if (c.cpf == null || c.cpf.length() != 11) {
+            System.out.println("CPF inválido.");
+            return false;
+        }
+
+        if (c.profissao == null || c.profissao.isEmpty()) {
+            System.out.println("Profissão inválida.");
+            return false;
+        }
+
+        if (c.estadoCivil == null || c.estadoCivil.isEmpty()) {
+            System.out.println("Estado civil inválido.");
+            return false;
+        }
+
+        if (c.nacionalidade == null || c.nacionalidade.isEmpty()) {
+            System.out.println("Nacionalidade inválida.");
+            return false;
+        }
+
+        if (c.enderecoTrabalho == null || c.enderecoTrabalho.isEmpty()) {
+            System.out.println("Endereço de trabalho inválido.");
+            return false;
+        }
+
+        return true;
     }
 }
